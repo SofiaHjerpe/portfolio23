07beab99575fb62f8d165404d5569a705e412bb3
SofiaHjerpe <hjerpesofia@gmail.com>
2023-04-20 11:52:05 +0200

    commit

---
+++ DisplayContacts/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MyPortfolio;
+
+Console.WriteLine("Welcome to Display Contacts!");
+Mongodb db = new Mongodb();
+
+var contacts = await db.GetAllContacts();
+
+foreach (var contact in contacts)
+{
+    Console.WriteLine(contact.Name);
+    Console.WriteLine(contact.Email);
+}
